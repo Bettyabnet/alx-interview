b055551e354f009c1b29d8337d6fbb285f2be771
bettyabnet <betelhemabnet1@gmail.com>
2024-04-19 00:17:43 +0300

    lock

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,18 +2,21 @@
 """
 A method that determines if all the boxes can be opened.
 """
+
+
 def canUnlockAll(boxes):
-     # Create a set to keep track of opened boxes
+
+    # Create a set to keep track of opened boxes
     opened_boxes = set()
     opened_boxes.add(0)  # Add the first box as opened
 
-     # Create a stack to perform DFS
+    # Create a stack to perform DFS
     stack = [0]
 
     while stack:
         current_box = stack.pop()
 
-    # Iterate through the keys in the current box
+        # Iterate through the keys in the current box
         for key in boxes[current_box]:
 
             if key < len(boxes) and key not in opened_boxes:
