779cd1e64c31f4b20214439a98e68d9b088fb232
bettyabnet <betelhemabnet1@gmail.com>
2024-08-24 22:33:32 +0300

    some adjestment

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,35 +1,16 @@
 #!/usr/bin/python3
+'''0x09. Island Perimeter'''
+
+
 def island_perimeter(grid):
-    """
-    Returns the perimeter of the island described in the input grid.
-
-    Args:
-        grid (list): A 2D list of integers representing the grid.
-                    0 represents water and 1 represents land.
-
-    Returns:
-        int: The perimeter of the island.
-    """
-    if not grid:
-        return 0
-
+    '''returns the perimeter of the island described in grid'''
     perimeter = 0
-    rows, cols = len(grid), len(grid[0])
-
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                # Check the top cell
-                if i == 0 or grid[i - 1][j] == 0:
-                    perimeter += 1
-                # Check the bottom cell
-                if i == rows - 1 or grid[i + 1][j] == 0:
-                    perimeter += 1
-                # Check the left cell
-                if j == 0 or grid[i][j - 1] == 0:
-                    perimeter += 1
-                # Check the right cell
-                if j == cols - 1 or grid[i][j + 1] == 0:
-                    perimeter += 1
-
+    for i, row in enumerate(grid):
+        for j, cell in enumerate(row):
+            if cell:
+                perimeter += 4
+                if i > 0 and grid[i-1][j]:
+                    perimeter -= 2
+                if j > 0 and row[j-1]:
+                    perimeter -= 2
     return perimeter
